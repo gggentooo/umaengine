545d57a42b20204b8ea4510652cdef8b3cb3e6bc
gggentooo <gggentooo@gmail.com>
2025-04-22 15:37:39 +0900

    created obj package

--- app/src/main/java/umaengine/App.java
+++ app/src/main/java/umaengine/App.java
@@ -3,6 +3,8 @@
  */
 package umaengine;
 
+import umaengine.obj.Race;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
@@ -10,5 +12,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        System.out.println(new Race().returnNum());
     }
 }
---
+++ app/src/main/java/umaengine/obj/Race.java
@@ -0,0 +1,18 @@
+/*
+ * Race.java
+ * 250422 gggentooo
+ */
+
+package umaengine.obj;
+
+public class Race {
+    private int num;
+
+    public Race() {
+        num = 8;
+    }
+
+    public int returnNum() {
+        return num;
+    }
+}
