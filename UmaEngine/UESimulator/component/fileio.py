9637a42c6e0c55cff6be61f183d9758483fa66cf
gggentooo <gggentooo@gmail.com>
2025-05-21 16:20:20 +0900

    added fileio content

--- UmaEngine/UESimulator/component/fileio.py
+++ UmaEngine/UESimulator/component/fileio.py
@@ -8,11 +8,14 @@ import json
 
 class JsonReader():
     def __init__(self):
+        self._data = None
         self._hasData = False
     
     def read(self, filepath: str):
         with open(filepath, "r", encoding="utf-8") as file:
             self._data = json.load(file)
+            if self._data != None:
+                self._hasData = True
     
     def clear_data(self):
         self._data = None
